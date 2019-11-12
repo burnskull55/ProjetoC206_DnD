c8d28df35bb5a9d5560f9a3e26f3217f3d034235
HumbertoG <51763770+burnskull55@users.noreply.github.com>
2019-11-11 22:02:54 -0300

    added dm model class

---
+++ ProjetoC206_DnD/src/br/inatel/proj/Model/DungeonMaster.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.inatel.proj.Model;
+
+/**
+ *
+ * @author burns
+ * @since  11/11/19
+ * @version 1.0
+ */
+public class DungeonMaster {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
